463498c3da308fcf590afa64d9015d876be44d23
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-01-12 00:00:49 +0100

    eleven

---
+++ index.js
@@ -0,0 +1,30 @@
+for (i = 0; i <= 4; i++){console.log(i);}
+const print = (str)=>console.log(str);
+print("asd");
+const nab = ["age", "user", "add"];
+console.log(nab);
+for (i=0; i< nab.length; i++){console.log(nab[i])};
+for (i=0; i< nab.length; i++){console.log(nab[i].toLocaleUpperCase())};
+for (i=1; i<=10; i++){console.log(i);}
+console.log(`${i} * 1 = ${i * 1}`);
+console.log(nab.length);
+const naba = ["hi", "iam", 123, true];
+console.log(typeof naba);
+console.log(nab[1]); 
+for (let i =0; i < nab.length; i++){console.log(nab[i]);};
+for ( let i = 1; i <=10; i++){console.log(`${i} * 1 = ${i * 1}`);}
+let str = " "
+for (let i=0; i < nab.length; i++)
+{str = nab[i][0].toUpperCase() + nab[i].substring(1); console.log(str);};
+let count = 0;
+if (2*5 == 8+9) {count++; console.log("yes you \n right");
+    
+} else {count--; console.log("no youre not \n right");
+    
+}
+let text = "";
+for (let i=0; i<=10; i++){if (i%2 ==0) {text += i + " even "};};
+console.log(text);
+let text1 = "";
+for (let i=1; i<=15; i++){if (i%3 ==0) {text1 += i + " odd "};};
+console.log(text1);
